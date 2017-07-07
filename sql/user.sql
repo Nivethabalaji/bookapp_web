0b39c33fdfccf829eb8732d189cd9e76eba0eba9
Nivethabalaji <balajinivetha05@gmail.com>
2017-07-07 13:55:59 +0530

    added functionality

---
+++ sql/user.sql
@@ -0,0 +1,9 @@
+create database book5;
+use book5;
+create table UserClass(id int primary key auto_increment,
+name varchar(20)not null,
+email varchar(20)not null, 
+password varchar(10)not null); 
+insert into UserClass values(12,'nivi','nivi05','12345');
+insert into UserClass values('manisha','manish05','1452');
+select * from UserClass;
